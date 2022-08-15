ceeebf382f14cc2224f59771d85bc525c7319a57
wEdoS <hoaisang0806@gmail.com>
2022-08-15 07:34:52 +0700

    config routes and layout

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Following from "~/pages/Following";
+import Home from "~/pages/Home";
+import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+import Search from "~/pages/Search";
+import { HeaderOnly } from "~/components/Layout";
+
+// *
+export const publicRoutes = [
+    { path: "/", component: Home },
+    { path: "/following", component: Following },
+    { path: "/profile", component: Profile },
+    { path: "/upload", component: Upload, layout: HeaderOnly },
+    { path: "/search", component: Search, layout: null },
+];
+
+// *
+export const privateRoutes = [];
